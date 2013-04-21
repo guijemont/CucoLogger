8c5562f818333dfe33eda174879088a4c0aaf84d
Guillaume Emont <guijemont@igalia.com>
2013-04-21 20:57:50 +0200

    cclogger: added configuration file

--- cclogger.py
+++ cclogger.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
 
+import parser, serial_tools, data_save
+
+class CCLoggerConfigException(Exception):
+    pass
+
 class CCLogger(object):
-    def __init__(self, source, parser, savers):
-        self._source = source
-        self._parser = parser
-        self._savers = savers
+    def __init__(self, config):
+        if "savers" not in config or len(config['savers']) == 0:
+            raise CCLoggerConfigException("no savers in config")
+
+        self._savers = []
+        for saver, saver_config in config['savers'].iteritems():
+            if saver == "RrdDataSaver":
+                constructor = data_save.RrdDataSaver
+            elif saver == "CsvDataSaver":
+                constructor = data_save.CsvDataSaver
+            else:
+                raise CCLoggerConfigException("Unknown saver: %s" % saver)
+            self._savers.append(constructor(**saver_config))
+
+        self._source = serial_tools.open_cc128()
+        self._parser = parser.CC128LiveParser()
 
     def run(self):
         try:
@@ -19,10 +36,17 @@ class CCLogger(object):
             saver.close()
 
 if __name__ == '__main__':
-    import sys, os
-    import parser, serial_tools, data_save
-    logger = CCLogger(serial_tools.open_cc128(),
-                parser.CC128LiveParser(),
-                (data_save.RrdDataSaver(sys.argv[1]),
-                data_save.CsvDataSaver(sys.argv[2], compress=True)))
+    import sys, os, json
+    config_file = "./cclogger.conf"
+    if len(sys.argv) > 1:
+        config_file = sys.argv[1]
+
+    if not os.path.exists(config_file):
+        print >> sys.stderr, "Could not find configuration file in %s, exiting" % config_file
+        sys.exit(1)
+
+    print "Loading configuration from %s" % config_file
+    config = json.load(open(config_file))
+
+    logger = CCLogger(config)
     logger.run()
